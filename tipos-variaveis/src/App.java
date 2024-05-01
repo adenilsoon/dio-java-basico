83f4b61b78c12232ba4ecc6b6d38d2d0f481d5b8
Adenilson Silva <sradenilson.sr16@gmail.com>
2024-05-01 15:04:48 -0300

    Escrita da classe app.java

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        /* O valor 2.500 em double não deve ser declarado com ponto ou virgula, apenas o valor bruto. Caso o valor possua centavos, aí sim coloca-se ponto como por exemplo 2500.65 (Dois mil  e quinhentos e sessenta e cinco centavos) */
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+        
+        // Problema de tipagem: conversão de shot para int e depois de int para short.
+         short numeroCurto = 1;
+         int numeroNormal = numeroCurto;
+         short numeroCurto2 = (short) numeroNormal;
+
     }
 }
